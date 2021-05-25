3ca527c355d0865fa1f5a979bf71b652047db159
ZeroDawn0D <umang26112003@gmail.com>
2021-05-25 15:03:50 +0530

    added pixi.js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import * as PIXI from "./pixi.mjs";
+let type = "WebGL"
+if(!PIXI.utils.isWebGLSupported()){
+  type = "canvas"
+}
+
+PIXI.utils.sayHello(type)
+
+//Create a Pixi Application
+let app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
