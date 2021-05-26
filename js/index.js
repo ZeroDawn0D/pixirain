c636f7aace15c25bb6d3d45d9d01af21e8f12ebf
ZeroDawn0D <umang26112003@gmail.com>
2021-05-27 02:28:55 +0530

    added rain rotation, added ui.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
-//import * as PIXI from "./pixi.mjs";
-import * as PixiRain from "./pixirain.js"
+import {createSpriteArray, startRain, app} from "./pixirain.js"
 
 
-document.body.appendChild(PixiRain.app.view);
+document.body.appendChild(app.view);
+let number = 4096;
+let colour = 0x555555;
+let rainH = 32;
+let rainW = 1;
+let dAngle = 60;
+let velocity = 15;
+createSpriteArray(number, colour, rainH, rainW, dAngle, velocity);
+startRain();
 
-//PixiRain.app.ticker.add(delta => PixiRain.gameLoop(delta))
-PixiRain.createSpriteArray(2048);
+
+
+//n = 2048, colour = 0x00ffff,  height = 16, width = 1, dAngle = 0, velocity = 2
--- js/pixirain.js
+++ js/pixirain.js
@@ -1,30 +1,34 @@
 import * as PIXI from "./pixi.mjs";
 //degree/radian conversions
-export const degToRad = (deg) => deg*Math.PI/180;
-export const radToDeg = (rad) => rad*180/Math.PI;
+const degToRad = (deg) => deg*Math.PI/180;
+const radToDeg = (rad) => rad*180/Math.PI;
+
+let wiw = window.innerWidth;
+let wih = window.innerHeight;
+
 
 // random integer in [min,max]
-export function getRandomInt(min, max) {
+function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 // angle wrt +ve y axis, negative of speed to reverse direction into upward
 // +ve Vy -> down, +ve Vx -> right
-export function updateVelocity(sprite, angle = 0, velocity = 2){
-  sprite.v = velocity * getRandomInt(5,20)/10;
-  sprite.vx = -sprite.v * Math.sin(angle);
-  sprite.vy = sprite.v * Math.cos(angle);
+function updateVelocity(sprite, rAngle = 0, velocity = 2){
+  sprite.v = velocity * getRandomInt(80,120)/100;
+  sprite.vx = -sprite.v * Math.sin(rAngle);
+  sprite.vy = sprite.v * Math.cos(rAngle);
 }
 
 //updates x,y of a sprite based on vx and vy;
-export function updatePosition(sprite){
+function updatePosition(sprite){
   sprite.x += sprite.vx;
   sprite.y += sprite.vy;
 
-  console.log(sprite.x, sprite.y);
+
 }
 
-export function createRainSprite(colour = 0x00ffff,x = 0, y = -5, height = 16, width = 1){
+function createRainSprite(colour, x, y, height, width, rAngle, velocity){
 	let sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
 	sprite.x = x;
 	sprite.y = y;
@@ -35,45 +39,74 @@ export function createRainSprite(colour = 0x00ffff,x = 0, y = -5, height = 16, w
 	sprite.anchor.x = sprite.anchor.y = 0.5;
 
 	sprite.v = sprite.vx = sprite.vy = 0;
-
-	updateVelocity(sprite);
+	sprite.rotation = rAngle;
+	updateVelocity(sprite, rAngle, velocity);
 
 	return sprite;
 }
 
 //create sprite array of size n
-export let spriteArray = new Array();
-export function createSpriteArray(n){
+let spriteArray = new Array();
+export function createSpriteArray(n = 2048, colour = 0x00ffff, height = 16, width = 1, dAngle = 0, velocity = 2){
+  let rAngle = degToRad(dAngle);
+  let leftLim = 0;
+  let rightLim = wiw;
+  if(rAngle >= 0){
+    rightLim += wih * Math.tan(rAngle);
+  }
+  else{
+  	leftLim += wih * Math.tan(rAngle);
+  }
   for(let i = 0; i < n; i++)
   {
-    spriteArray.push(createRainSprite());
-    spriteArray[i].y = getRandomInt(-100,0)
-    spriteArray[i].x = getRandomInt(0,2000);
+    spriteArray.push(createRainSprite(
+      colour,
+      getRandomInt(leftLim,rightLim), 
+      getRandomInt(-wih,0), 
+      height, 
+      width, 
+      rAngle,
+      velocity)
+    );
+    
+    //spriteArray[i].y = getRandomInt(-100,0)
     app.stage.addChild(spriteArray[i]);
   }
 }
 
-export function gameLoop(delta) {
+function gameLoop(delta) {
 	for(let i = 0; i < spriteArray.length; i++){
 	  updatePosition(spriteArray[i])
 	  if(spriteArray[i].y > app.renderer.height +  10){
-	  	spriteArray[i].y = getRandomInt(-100,0);
-        spriteArray[i].x = getRandomInt(0,2000);
+	  	let rAngle = spriteArray[i].rotation;
+	  	let leftLim = 0;
+        let rightLim = wiw;
+        if(rAngle >= 0){
+          rightLim += wih * Math.tan(rAngle);
+        }
+        else{
+  	      leftLim += wih * Math.tan(rAngle);
+        }
+
+	  	spriteArray[i].y = getRandomInt(-wih,0);
+        spriteArray[i].x = getRandomInt(leftLim,rightLim);
 	  }
 	}
 }
 
-export function onMenuIconLoaded(){
-  
-}
 
 export let app = new PIXI.Application({
-  width: window.innerWidth, 
-  height: window.innerHeight,
+  width: wiw, 
+  height: wih,
   backgroundColor: 0x000000
 });
 
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
-app.ticker.add(delta => gameLoop(delta))
+
+
+//adds gameLoop() to app.ticker()
+export function startRain(){
+	app.ticker.add(delta => gameLoop(delta))
+}
